86e06fb515d25804089c977e5273cda86297cf18
Ilya Shevelev <robotedu@yandex.ru>
2023-12-26 20:09:55 +0300

    feat: w/o static

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -147,4 +148,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 2
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
